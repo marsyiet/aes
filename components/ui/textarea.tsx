cf5c5fbeda716014ecf1a969f1fd5aa896dc8982
marsyiet <etoundimarius237@gmail.com>
2024-12-10 06:46:14 +0100

    ...

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -9,8 +9,8 @@ const Textarea = React.forwardRef<
   return (
     <textarea
       className={cn(
-        "flex h-2rem text-sm lg:text-4xl w-full rounded-xl border border-muted-foreground/20 bg-white/5 text-white px-2 lg:px-10 py-2 lg:py-5 shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-2xl",
-        className
+        "flex h-[10rem] text-sm lg:text-4xl w-full rounded-xl border border-muted-foreground/20 bg-white/5 text-white px-2 lg:px-10 py-2 lg:py-5 shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-2xl",
+        className 
       )}
       ref={ref}
       {...props}
